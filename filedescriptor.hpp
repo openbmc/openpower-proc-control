233b3edaa71a2fbb7f6d9dda824402422e98140c
Matt Spinler <spinler@us.ibm.com>
2017-02-28 10:02:16 -0600

    Add FileDescriptor class
    A simple class to wrap a file descriptor so it can
    be opened on construction and closed on destruction.
    
    Change-Id: I16d481fdc91e99720acd9cabff3a3a4690d8a573
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

---
+++ filedescriptor.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <fcntl.h>
+#include <string>
+
+namespace openpower
+{
+namespace p9_util
+{
+
+/**
+ * Class to wrap a file descriptor.
+ *
+ * Opens the descriptor in the constructor, and
+ * then closes it when destroyed.
+ */
+class FileDescriptor
+{
+    public:
+
+        FileDescriptor() = delete;
+        FileDescriptor(const FileDescriptor&) = delete;
+        FileDescriptor(FileDescriptor&&) = default;
+        FileDescriptor& operator=(const FileDescriptor) = delete;
+        FileDescriptor& operator=(FileDescriptor&&) = default;
+
+        /**
+         * Creates a file descriptor by opening the device
+         * path passed in.
+         *
+         * @param path[in] - the device path that will be open
+         */
+        FileDescriptor(const std::string& path);
+
+        /**
+         * Closes the file.
+         */
+        ~FileDescriptor();
+
+        /**
+         * The method to access the file descriptor value
+         */
+        inline int operator()() const
+        {
+            return fd;
+        }
+
+    private:
+
+        /**
+         * The actual file descriptor
+         */
+        int fd;
+};
+
+}
+}
