3f8ee850c7c76d4d4140238bc3790d8ab46f5a22
spashabk-in <shakeebbk@in.ibm.com>
2017-09-19 04:11:38 -0500

    Log istep info on watchdog timeout
    1. SBE istep info (cfam 2809)
    2. HB istep info (cfam 283C)
    
    Change-Id: I67632c58fe1148b980791dc017e5c099107caae5
    Signed-off-by: Shakeeb Pasha <shakeebbk@in.ibm.com>

--- p9_cfam.hpp
+++ p9_cfam.hpp
@@ -16,8 +16,10 @@ static constexpr uint16_t P9_FSI2PIB_INTERRUPT     = 0x100B;
 static constexpr uint16_t P9_FSI2PIB_TRUE_MASK     = 0x100D;
 static constexpr uint16_t P9_CBS_CS                = 0x2801;
 static constexpr uint16_t P9_SBE_CTRL_STATUS       = 0x2808;
+static constexpr uint16_t P9_SBE_MSG_REGISTER      = 0x2809;
 static constexpr uint16_t P9_ROOT_CTRL0            = 0x2810;
 static constexpr uint16_t P9_PERV_CTRL0            = 0x281A;
+static constexpr uint16_t P9_HB_MBX5_REG           = 0x283C;
 static constexpr uint16_t P9_SCRATCH_REGISTER_8    = 0x283F;
 static constexpr uint16_t P9_ROOT_CTRL8            = 0x2918;
 }
