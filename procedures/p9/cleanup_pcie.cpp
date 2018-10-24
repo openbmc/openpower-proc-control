f25f60ec510796d80b9459118b8b667f3f6bd73e
Anthony Wilson <wilsonan@us.ibm.com>
2018-10-24 16:37:50 -0500

    Add cleanupPcie to openpower-proc-control
    Enables systems without pdbg to perform the same
    function of:
        `pdbg -p 0 putcfam 0x2931 0x00001C00`
    
    Change-Id: I6e027260f78a3fc451a45832f6f9bcf9afc8c3b9
    Signed-off-by: Anthony Wilson <wilsonan@us.ibm.com>

---
+++ procedures/p9/cleanup_pcie.cpp
@@ -0,0 +1,52 @@
+/**
+ * Copyright © 2018 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <phosphor-logging/log.hpp>
+#include "cfam_access.hpp"
+#include "p9_cfam.hpp"
+#include "registration.hpp"
+#include "targeting.hpp"
+#include "ext_interface.hpp"
+
+namespace openpower
+{
+namespace p9
+{
+
+using namespace phosphor::logging;
+using namespace openpower::cfam::access;
+using namespace openpower::cfam::p9;
+using namespace openpower::targeting;
+
+/**
+ * @brief POWER9 PCIe Power-off Workaround
+ * @return void
+ */
+void cleanupPcie()
+{
+    Targeting targets;
+    const auto& master = *(targets.begin());
+
+    log<level::INFO>("Running P9 procedure cleanupPcie",
+                     entry("NUM_PROCS=%d", targets.size()));
+
+    // Perform PCIE power-off workaround
+    writeReg(master, P9_ROOT_CTRL1_CLEAR, 0x00001C00);
+}
+
+REGISTER_PROCEDURE("cleanupPcie", cleanupPcie);
+
+}
+}
