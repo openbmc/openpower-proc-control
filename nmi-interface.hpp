c3f96a2f46cc63e1f3b6f9c8aa9758f090e80ab4
Lakshminarayana R. Kammath <lkammath@in.ibm.com>
2019-06-03 04:33:33 -0500

    sreset control code enablement
    Signed-off-by: Lakshminarayana R. Kammath <lkammath@in.ibm.com>
    Change-Id: I16f3bb2a2ed0c0ffcea2a720a2ae39a2b303ef9e

---
+++ nmi-interface.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <xyz/openbmc_project/Control/Host/NMI/server.hpp>
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/server/object.hpp>
+#include <sys/inotify.h>
+#include <systemd/sd-event.h>
+
+namespace openpower
+{
+namespace proc
+{
+
+using Base = sdbusplus::xyz::openbmc_project::Control::Host::server::NMI;
+using Interface = sdbusplus::server::object::object<Base>;
+namespace sdbusRule = sdbusplus::bus::match::rules;
+
+/* Need a custom deleter for freeing up sd_event */
+struct EventDeleter
+{
+    void operator()(sd_event* event) const
+    {
+        event = sd_event_unref(event);
+    }
+};
+
+using EventPtr = std::unique_ptr<sd_event, EventDeleter>;
+
+
+/*  @class NMI
+ *  @brief Implementation of NMI (SRESET PROC)
+ */
+class NMI : public Interface
+{
+  public:
+    NMI() = delete;
+    NMI(const NMI&) = delete;
+    NMI& operator=(const NMI&) = delete;
+    NMI(NMI&&) = delete;
+    NMI& operator=(NMI&&) = delete;
+    virtual ~NMI() = default;
+
+    /*  @brief Constructor to put object onto bus at a dbus path.
+     *  @param[in] bus - sdbusplus D-Bus to attach to.
+     *  @param[in] path - Path to attach to.
+     */
+    NMI(sdbusplus::bus::bus& bus, const char* path);
+
+
+    /* NMI(sdbusplus::bus::bus& bus, const EventPtr& event, const char* path) :
+        Interface(bus, path), bus(bus), eventP(event.get())
+    {
+    }*/
+
+    /*  @brief do sreset.
+     */
+    void nmiReset() override;
+  private:
+
+    /** @brief sdbus handle */
+    sdbusplus::bus::bus& bus;
+    
+    /** @brief sdbusplus event loop */
+    EventPtr eventP;
+
+    /** @brief object path */
+    std::string objectPath;
+
+};
+
+} // namespace proc
+} // namespace open_power
+
