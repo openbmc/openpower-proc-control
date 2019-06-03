aac0804106ce77aabbdbadf485814624e3b9c368
Lakshminarayana R. Kammath <lkammath@in.ibm.com>
2019-06-03 04:33:33 -0500

    sreset control code enablement
    Signed-off-by: Lakshminarayana R. Kammath <lkammath@in.ibm.com>
    Change-Id: I16f3bb2a2ed0c0ffcea2a720a2ae39a2b303ef9e

---
+++ soft_reset.cpp
@@ -0,0 +1,104 @@
+/**
+ * Copyright (C) 2019 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "soft_reset.hpp"
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/elog.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+#include "targeting.hpp"
+#include <sbe_interfaces.hpp>
+
+namespace openpower
+{
+namespace proc
+{
+
+using namespace phosphor::logging;
+using sdbusplus::exception::SdBusError;
+using InternalFailure = sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
+
+SoftReset::SoftReset(sdbusplus::bus::bus& bus, const char* path):
+    Interface(bus, path),
+    bus(bus), objectPath(path)
+{
+
+    this->emit_object_added();
+
+}
+
+void SoftReset::reset()
+{
+    using namespace openpower::targeting;
+    using namespace openpower::sbe::threadcontrol;
+    constexpr auto PROC0_SBE_FIFO_PATH = "/dev/sbefifo1";
+    constexpr auto PROC1_SBE_FIFO_PATH = "/dev/sbefifo2";
+
+    Targeting targets;
+
+    // STOP followed by SRESET on all processors,cores & threads on the system
+    for (const auto& proc : targets)
+    {
+        try
+        {
+            if (proc->getPos() == 0){
+                stop(PROC0_SBE_FIFO_PATH,0xFF,0xF);
+                sreset(PROC0_SBE_FIFO_PATH, 0xFF, 0xF);
+            }
+            else if (proc->getPos() == 1){
+                stop(PROC1_SBE_FIFO_PATH,0xFF,0xF);
+                sreset(PROC1_SBE_FIFO_PATH, 0xFF, 0xF);
+            }
+            else{
+                log<level::ERR>("Found unknown PROC target",
+                                entry("PROC=%d", proc->getPos()));
+            }
+
+            log<level::INFO>("Executed SBE Reset successfully",
+                              entry("PROC=%d", proc->getPos()));
+        }
+        catch (const std::runtime_error& e)
+        {
+            log<level::ERR>(e.what());
+            elog<InternalFailure>();
+            // We want to continue - capturing as much info as possible
+        }
+    }
+
+}
+
+#if 0
+void SoftReset::reset()
+{
+
+    auto method = bus.new_method_call(SYSTEMD_BUSNAME, SYSTEMD_PATH,
+                                      SYSTEMD_INTERFACE, "StartUnit");
+    method.append("sreset.service", "replace");
+    try
+    {
+        auto reply = bus.call_noreply(method);
+    }
+    catch (const SdBusError& e)
+    {
+        log<level::ALERT>("Error in trying to SRESET. ");
+        report<InternalFailure>();
+    }
+
+}
+#endif
+
+
+} // namespace proc
+} // namespace openpower
