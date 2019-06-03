28d027dcde67c5e478ddbe90411c9d011dec08dc
Lakshminarayana R. Kammath <lkammath@in.ibm.com>
2019-06-03 04:33:33 -0500

    sreset control code enablement
    Signed-off-by: Lakshminarayana R. Kammath <lkammath@in.ibm.com>
    Change-Id: I16f3bb2a2ed0c0ffcea2a720a2ae39a2b303ef9e

---
+++ mainapp.cpp
@@ -0,0 +1,38 @@
+#include "nmi-interface.hpp"
+#include <sdbusplus/bus.hpp>
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/elog.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+
+int main(int argc, char* argv[])
+{
+
+    using namespace phosphor::logging;
+    constexpr auto BUSPATH_SRESET_CONFIG = "/xyz/openbmc_project/control/host0/nmi";
+    constexpr auto BUSNAME_SRESET_CONFIG = "xyz.openbmc_project.Control.Host.NMI";
+    auto bus = sdbusplus::bus::new_default();
+
+    // Add sdbusplus ObjectManager
+    sdbusplus::server::manager::manager objManager(bus, BUSPATH_SRESET_CONFIG);
+
+    try
+    {
+        openpower::proc::NMI NMI(bus,
+        BUSPATH_SRESET_CONFIG);
+
+        bus.request_name(BUSNAME_SRESET_CONFIG);
+    }
+
+    catch (const std::runtime_error& e)
+    {
+        log<level::ERR>("Error in trying to SRESET service. ");
+    }
+
+    while (true)
+    {
+        bus.process_discard();
+        bus.wait();
+    }
+
+    return 0;
+}
