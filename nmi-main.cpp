39a563ac5a0d38af1d59e1b4cc4159773b0e5a84
Lakshminarayana R. Kammath <lkammath@in.ibm.com>
2019-06-03 04:33:33 -0500

    NMI (soft reset) control code enablement
    Changes here include:
       1) A service that triggers Open BMC App (openpower-proc-nmi)
       2) An application that waits for a dbus event (NMI) to occur
       3) A reset logic that triggers a NMI/softreset dbus service which
          will invoke pdbg call to trigger stop followed by sreset on all
          threads
       4) Necessary Makefile.am changes
    
    Tested: Working Good
    
    Signed-off-by: Lakshminarayana R. Kammath <lkammath@in.ibm.com>
    Change-Id: I16f3bb2a2ed0c0ffcea2a720a2ae39a2b303ef9e

---
+++ nmi-main.cpp
@@ -0,0 +1,72 @@
+/**
+ * Copyright © 2019 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "nmi-interface.hpp"
+
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/elog.hpp>
+#include <sdbusplus/bus.hpp>
+
+int main(int argc, char* argv[])
+{
+
+    using namespace phosphor::logging;
+    using InternalFailure =
+        sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
+
+    constexpr auto BUSPATH_NMI = "/xyz/openbmc_project/control/host0/nmi";
+    constexpr auto BUSNAME_NMI = "xyz.openbmc_project.Control.Host.NMI";
+    auto bus = sdbusplus::bus::new_default();
+    sd_event* event = nullptr;
+    auto rc = sd_event_default(&event);
+
+    if (rc < 0)
+    {
+        log<level::ERR>("Error occurred during the sd_event_default",
+                        entry("RC=%d", rc));
+        report<InternalFailure>();
+        return rc;
+    }
+
+    // Initialize the event
+    openpower::proc::EventPtr eventP{event};
+    event = nullptr;
+
+    // Add sdbusplus ObjectManager
+    sdbusplus::server::manager::manager objManager(bus, BUSPATH_NMI);
+    bus.request_name(BUSNAME_NMI);
+
+    try
+    {
+        openpower::proc::NMI NMI(bus, BUSPATH_NMI);
+        bus.attach_event(eventP.get(), SD_EVENT_PRIORITY_NORMAL);
+
+        auto rc = sd_event_loop(eventP.get());
+        if (rc < 0)
+        {
+            log<level::ERR>("Error occurred during the sd_event_loop",
+                            entry("RC=%d", rc));
+            elog<InternalFailure>();
+        }
+    }
+
+    catch (InternalFailure& e)
+    {
+        commit<InternalFailure>();
+        return -1;
+    }
+    return 0;
+}
