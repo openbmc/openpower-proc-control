75b10e5b89443c16ae4abd4386a84bec3214aad9
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-07-17 02:24:53 -0500

    Open power boot manager implementation
    First commit of the open power boot manager implementation
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Icc01c0bd5859d07abf4f8f0b2e54e0f461579be8

---
+++ boot_manager.cpp
@@ -0,0 +1,15 @@
+#include "boot_manager.hpp"
+
+namespace open_power
+{
+namespace boot
+{
+
+void Manager::doIpl(uint32_t stepMajor, uint32_t stepMinor)
+{
+   return; 
+}
+
+
+} // namespace boot
+} // namespace open_power
---
+++ boot_manager_main.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <iostream>
+#include <exception>
+#include <sdbusplus/bus.hpp>
+#include "boot_manager.hpp"
+
+constexpr auto BOOT_OBJPATH="/org/open_power/state/boot";
+constexpr auto BOOT_BUSNAME="org.open_power.State.Boot";
+
+int main(int argc, char *argv[])
+{
+
+    auto bus = sdbusplus::bus::new_default();
+
+    // For now, we only have one instance of the host
+    auto objPathInst = std::string{BOOT_OBJPATH} + '0';
+
+    // Add sdbusplus ObjectManager.
+    sdbusplus::server::manager::manager objManager(bus, objPathInst.c_str());
+
+    open_power::boot::Manager manager(bus, objPathInst.c_str());
+
+    bus.request_name(BOOT_BUSNAME);
+
+    while (true)
+    {
+        bus.process_discard();
+        bus.wait();
+    }
+    return 0;
+}
