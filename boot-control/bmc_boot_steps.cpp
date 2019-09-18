06444acf73fafb4c5a6f3eff9befccf73ac22ec8
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-09-18 06:09:54 -0500

    Add support for BMC poweron istep
    Add support for BMC poweron istep by setting chassis on target
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ieddd45f679ad18a5a624bf2ae710f2d80eb281e0

--- boot-control/bmc_boot_steps.cpp
+++ boot-control/bmc_boot_steps.cpp
@@ -1,6 +1,13 @@
 #include "bmc_boot_steps.hpp"
 
+#include "util.hpp"
+#include "xyz/openbmc_project/Common/error.hpp"
+
+#include <unistd.h>
+
+#include <chrono>
 #include <iostream>
+#include <phosphor-logging/elog-errors.hpp>
 
 namespace openpower
 {
@@ -8,6 +15,30 @@ namespace boot
 {
 namespace bmc_steps
 {
+using namespace phosphor::logging;
+using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+
+void powerOn()
+{
+    // Poweron the chassis
+    util::chassisPowerOn();
+
+    auto sleepTime = std::chrono::seconds(5);
+    auto timeout = std::chrono::seconds(20);
+    auto now = std::chrono::system_clock::now();
+    while (std::chrono::system_clock::now() < (now + timeout))
+    {
+        if (util::isChassisOn())
+        {
+            return;
+        }
+        std::this_thread::sleep_for(sleepTime);
+        sleepTime = std::chrono::seconds(2);
+    }
+
+    log<level::ERR>("Timeout encountered while waiting for chassis on");
+    elog<InternalFailure>();
+}
 
 void stubbedStep()
 {
---
+++ boot-control/util.hpp
@@ -0,0 +1,38 @@
+#include <sdbusplus/server.hpp>
+namespace openpower
+{
+namespace boot
+{
+namespace util
+{
+
+/**
+ * @brief Get the bus service
+ * @param[in] bus - Bus handler
+ * @param[in] path - Object Path
+ * @param[in] intrtface - Interface
+ *
+ * @return the bus service as a string
+ * @error InternalFailure exception thrown
+ */
+std::string getService(sdbusplus::bus::bus& bus, const std::string& path,
+                       const std::string& interface);
+
+/**
+ * @brief Check whether chassis is on
+ *
+ * @return true if chassis is on
+ * @error InternalFailure exception thrown
+ */
+bool isChassisOn();
+
+/**
+ * @brief Power on chassis
+ *
+ * @error InternalFailure exception thrown
+ */
+void chassisPowerOn();
+
+} // namespace util
+} // namespace boot
+} // namespace openpower
