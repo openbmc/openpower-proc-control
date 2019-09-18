bb3523a84216f4aa03ddd2a711aa5126d2b12708
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-09-18 05:39:48 -0500

    Add Argument parser to openpower-boot-control.
    Add support to pass arguments to the app.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: If46531abde2fd9638d31dbe884ce78a5bf7273fc

---
+++ argument.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <getopt.h>
+#include <map>
+#include <string>
+
+namespace open_power
+{
+namespace boot
+{
+namespace util
+{
+
+/**
+ * @brief enum - Booting modes
+ */
+enum class ExecMode
+{
+    INVALID, //Invalid mode
+    STEP,    //Step by step mode
+    NORMAL   //Normal booting
+};
+struct optstruct
+{
+    ExecMode mode; //Boot mode
+    uint8_t start_major; //Starting major step
+    uint8_t end_major; //Ending major step
+    uint8_t start_minor; //Starting minor step
+    uint8_t end_minor; //Ending minor step
+};
+
+/**
+ * @brief Class - Encapsulates parsing command line options and
+ *                populating arguments.
+ */
+class ArgumentParser
+{
+  public:
+    ArgumentParser(int argc, char** argv);
+    ArgumentParser() = delete;
+    ArgumentParser(const ArgumentParser&) = delete;
+    ArgumentParser(ArgumentParser&&) = default;
+    ArgumentParser& operator=(const ArgumentParser&) = delete;
+    ArgumentParser& operator=(ArgumentParser&&) = default;
+    ~ArgumentParser() = default;
+    const std::string& operator[](const std::string& opt);
+
+    static void usage(char** argv);
+
+    static const std::string true_string;
+    static const std::string empty_string;
+
+  private:
+    std::map<const std::string, std::string> arguments;
+
+    static const option options[];
+    static const char* optionstr;
+};
+
+/**
+ * @brie parse options to a structure
+ */
+void parseArguments(int argc, char** argv, optstruct &opt);
+
+} // namespace util
+} // namespace boot
+} // namespace open_power
--- boot_control_main.cpp
+++ boot_control_main.cpp
@@ -1,11 +1,15 @@
 #include "boot_control.hpp"
+#include "argument.hpp"
 
 #include <iostream>
 
 int main(int argc, char** argv)
 {
     int rc = -1;
+    open_power::boot::util::optstruct opt;
+    open_power::boot::util::parseArguments(argc, argv, opt);
+
     open_power::boot::Control ctrl(0,0);
-    rc = ctrl.executeStep(0,0);
+    rc = ctrl.executeStep(opt.start_major, opt.start_minor);
     return rc;
 }
