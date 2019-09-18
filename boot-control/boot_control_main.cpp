48e32a6850b77fcc1cfcdbf579b272d6ac567236
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-09-18 06:22:27 -0500

    Support for executing a range of isteps
    Support for execute range of major steps by specifying
    start and end steps.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Iafe0b5e2b8811a10ee6573fd93bc42cf4a5fe40a

--- boot-control/boot_control_main.cpp
+++ boot-control/boot_control_main.cpp
@@ -3,43 +3,81 @@
 
 #include <CLI/CLI.hpp>
 #include <iostream>
+#include <optional>
 #include <phosphor-logging/elog-errors.hpp>
+
 int main(int argc, char** argv)
 {
     using namespace phosphor::logging;
     using namespace sdbusplus::xyz::openbmc_project::Common::Error;
     CLI::App app{"OpenPOWER boot control"};
 
+    std::string normalOptions = "Option specify single boot step";
     // Boot step major number
-    std::string major;
-    app.add_option("-m,--major", major, "Step Major Number")->required();
+    std::optional<std::string> major;
+    app.add_option("-m,--major", major, "Step Major Number")
+        ->group(normalOptions);
 
     // Boot step minor number
-    std::string minor;
-    app.add_option("-i,--minor", minor, "Step Minor Number")->required();
+    std::optional<std::string> minor;
+    app.add_option("-i,--minor", minor, "Step Minor Number")
+        ->group(normalOptions);
+
+    std::string rangeOptions = "Option to specify range of boot steps";
+
+    std::optional<std::string> steps;
+    app.add_option(
+           "-s, --step", steps,
+           "Steps to be exceuted, range of steps to be seperated by '..'")
+        ->group(rangeOptions);
     CLI11_PARSE(app, argc, argv);
 
-    uint8_t major_number = 0;
-    uint8_t minor_number = 0;
+    openpower::boot::Control ctrl;
+
     try
     {
-        major_number = std::stoi(major);
-        minor_number = std::stoi(minor);
+        if ((minor) && (major))
+        {
+            uint8_t major_number = 0;
+            uint8_t minor_number = 0;
+            major_number = std::stoi(*major);
+            minor_number = std::stoi(*minor);
 
-        openpower::boot::Control ctrl;
-        ctrl.executeStep(major_number, minor_number);
+            ctrl.executeStep(major_number, minor_number);
+        }
+        else if (steps)
+        {
+            int start_major = 0;
+            int end_major = 0;
+            auto splitPos = steps->find("..");
+            if (splitPos == std::string::npos)
+            {
+                start_major = std::stoi(*steps);
+                end_major = start_major;
+            }
+            else
+            {
+                start_major = std::stoi(steps->substr(0, splitPos));
+                end_major = std::stoi(steps->substr(splitPos + 2));
+            }
+
+            ctrl.executeRange(start_major, end_major);
+        }
     }
     catch (const InternalFailure& e)
     {
-        std::cerr << "Error in executing the step " << e.what() << std::endl;
+        std::cerr << "Error in executing the boot steps " << e.what()
+                  << std::endl;
         commit<InternalFailure>();
         exit(-1);
     }
     catch (std::exception& e)
     {
-        std::cerr << "Error in executing the step " << e.what() << std::endl;
+        std::cerr << "Executing the boot steps failed " << e.what()
+                  << std::endl;
         report<InternalFailure>();
         exit(-1);
     }
+
     exit(0);
 }
