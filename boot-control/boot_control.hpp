61f3c66fe728e542c63a5696275f1e6d7cac68dc
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-09-18 06:22:27 -0500

    Support for executing a range of isteps
    Support for execute range of major steps by specifying
    start and end steps.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Iafe0b5e2b8811a10ee6573fd93bc42cf4a5fe40a

--- boot-control/boot_control.hpp
+++ boot-control/boot_control.hpp
@@ -13,6 +13,12 @@ namespace boot
 using BmcExecList = std::map<uint8_t, std::function<int(void)>>;
 using BmcStepList = std::map<uint8_t, BmcExecList>;
 
+using StepName = std::string;
+using MajorStepNumber = uint8_t;
+using MinorStepNumber = uint8_t;
+using MinorStepList = std::map<MinorStepNumber, StepName>;
+using MajorStepsList = std::map<MajorStepNumber, MinorStepList>;
+
 /** @class Control
  *  @brief Implements boot step control
  *  @details Entry point for executing boot steps and execute the step on
@@ -33,8 +39,17 @@ class Control
      *  @param[in] stepMinor - a Minor boot step or substep.
      *  @return    zero if success.
      */
+
     int executeStep(uint8_t stepMajor, uint8_t stepMinor);
+
+    /*  @brief Execute a range of boot step.
+     *  @param[in] startStep - a Major start boot step.
+     *  @param[in] stepMinor - a Major end boot step.
+     */
+    int executeRange(uint8_t startStep, uint8_t endStep);
+
     static BmcStepList bmcSteps;
+    static MajorStepsList majorSteps;
 
   private:
     /** @brief Execute a boot step in BMC.
