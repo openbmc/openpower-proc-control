b6509eed208228cd58ef949430695bcd60f8e6f7
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-09-18 05:29:35 -0500

    Initial commit for boot control application
    This is the skelton of the boot control app.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ia44ea8641bf9f09d2dc059dfffcbc5ebe8c71e00

---
+++ boot-control/boot_control.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <stdint.h>
+
+namespace open_power
+{
+namespace boot
+{
+
+class Control
+{
+  public:
+    Control(const Control&) = delete;
+    Control& operator=(const Control&) = delete;
+    Control(Control&&) = delete;
+    Control& operator=(Control&&) = delete;
+    virtual ~Control() = default;
+
+    Control()
+    {
+    }
+
+    /*  @brief Execute a boot step.
+     *  @param[in] stepMajor - a Major boot step.
+     *  @param[in] stepMinor - a Minor boot step or substep.
+     */
+    int executeStep(uint8_t stepMajor, uint8_t stepMinor);
+
+  private:
+    int executeBmcStep(uint8_t stepMajor, uint8_t stepMinor);
+    int executeHostStep(uint8_t stepMajor, uint8_t stepMinor);
+};
+} // namespace boot
+} // namespace open_power
