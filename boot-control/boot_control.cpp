26fb38712bb0b490568a3196023e10cc2869c63e
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-09-18 06:22:27 -0500

    Support for executing a range of isteps
    Support for execute range of major steps by specifying
    start and end steps.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Iafe0b5e2b8811a10ee6573fd93bc42cf4a5fe40a

--- boot-control/boot_control.cpp
+++ boot-control/boot_control.cpp
@@ -20,6 +20,8 @@ BmcStepList Control::bmcSteps = {
       {6, []() { return bmc_steps::StubbedStep(); }},
       {7, []() { return bmc_steps::StubbedStep(); }}}}};
 
+MajorStepsList Control::majorSteps = {{0, {{0, "poweron"}}}};
+
 int Control::executeHostStep(uint8_t stepMajor, uint8_t stepMinor)
 {
     return 0;
@@ -71,5 +73,42 @@ int Control::executeStep(uint8_t stepMajor, uint8_t stepMinor)
     return rc;
 }
 
+int Control::executeRange(uint8_t startStep, uint8_t endStep)
+{
+    int rc = -1;
+
+    auto begin_step = majorSteps.find(startStep);
+    if (begin_step == majorSteps.end())
+    {
+        std::cout << "Invalid start step :" << startStep << std::endl;
+        return -1;
+    }
+
+    auto end_step = majorSteps.find(endStep);
+    if (end_step == majorSteps.end())
+    {
+        std::cout << "Invalid end step :" << endStep << std::endl;
+        return -1;
+    }
+    for (auto iter = begin_step; iter != majorSteps.end(); iter++)
+    {
+        for (auto miter = iter->second.begin(); miter != iter->second.end();
+             miter++)
+        {
+            std::cout << "Executing:" << miter->second << std::endl;
+            rc = executeStep(iter->first, miter->first);
+            if (rc)
+            {
+                return rc;
+            }
+        }
+
+        if (endStep == iter->first)
+        {
+            break;
+        }
+    }
+    return rc;
+}
 } // namespace boot
 } // namespace open_power
