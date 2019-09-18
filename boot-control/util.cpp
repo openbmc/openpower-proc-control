33820c3b5eac425cb6a632e8068fcee093a096b6
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-09-18 06:09:54 -0500

    Add support for BMC poweron istep
    Add support for BMC poweron istep by setting chassis on target
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ieddd45f679ad18a5a624bf2ae710f2d80eb281e0

---
+++ boot-control/util.cpp
@@ -0,0 +1,95 @@
+#include "util.hpp"
+
+#include "xyz/openbmc_project/Common/error.hpp"
+
+#include <phosphor-logging/elog-errors.hpp>
+
+#include "../config.h"
+namespace open_power
+{
+namespace boot
+{
+namespace util
+{
+
+using namespace phosphor::logging;
+using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+
+std::string getService(sdbusplus::bus::bus& bus, const std::string& path,
+                       const std::string& interface)
+{
+    auto mapperCall = bus.new_method_call(MAPPER_BUSNAME, MAPPER_PATH,
+                                          MAPPER_INTERFACE, "GetObject");
+    mapperCall.append(path, std::vector<std::string>({interface}));
+    std::map<std::string, std::vector<std::string>> mapperResponse;
+    try
+    {
+        auto mapperResponseMsg = bus.call(mapperCall);
+        mapperResponseMsg.read(mapperResponse);
+        if (mapperResponse.empty())
+        {
+            log<level::ERR>("Invalid Response from mapper",
+                            entry("PATH=%s", path.c_str()),
+                            entry("interface=%s", interface.c_str()));
+            elog<InternalFailure>();
+        }
+    }
+    catch (const sdbusplus::exception::SdBusError& e)
+    {
+        log<level::ERR>("Error in Mapper call");
+        elog<InternalFailure>();
+    }
+
+    return mapperResponse.begin()->first;
+}
+
+int chassisPowerOn()
+{
+    sdbusplus::bus::bus bus = sdbusplus::bus::new_default();
+
+    try
+    {
+        auto method = bus.new_method_call(SYSTEMD_SERVICE, SYSTEMD_OBJ_PATH,
+                                          SYSTEMD_INTERFACE, "StartUnit");
+        method.append(CHASSIS_ON_TGT);
+        method.append("replace");
+        bus.call_noreply(method);
+    }
+    catch (const sdbusplus::exception::SdBusError& e)
+    {
+        log<level::ERR>("Error in Chassis Power On");
+        elog<InternalFailure>();
+    }
+    return 0;
+}
+
+bool isChassisOn()
+{
+    sdbusplus::bus::bus bus = sdbusplus::bus::new_default();
+
+    auto service = getService(bus, CHASSIS_STATE_PATH, CHASSIS_STATE_OBJ);
+
+    auto method = bus.new_method_call(service.c_str(), CHASSIS_STATE_PATH,
+                                      SYSTEMD_PROPERTY_INTERFACE, "Get");
+    method.append(CHASSIS_STATE_OBJ, "CurrentPowerState");
+
+    sdbusplus::message::variant<std::string> currentChassisState;
+
+    try
+    {
+        auto response = bus.call(method);
+        response.read(currentChassisState);
+        auto strParam = sdbusplus::message::variant_ns::get<std::string>(
+            currentChassisState);
+        return (strParam != CHASSIS_STATE_OFF);
+    }
+    catch (const sdbusplus::exception::SdBusError& e)
+    {
+        log<level::ERR>("Error in fetching current Chassis State");
+        elog<InternalFailure>();
+    }
+}
+
+} // namespace util
+} // namespace boot
+} // namespace open_power
