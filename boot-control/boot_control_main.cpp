28465b5a09cbefab2b8db73d7e27a7288302cd82
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-09-18 05:39:48 -0500

    Add Argument parser to openpower-boot-control.
    Add support to pass arguments to the app.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: If46531abde2fd9638d31dbe884ce78a5bf7273fc

--- boot-control/boot_control_main.cpp
+++ boot-control/boot_control_main.cpp
@@ -1,17 +1,39 @@
 #include "boot_control.hpp"
+#include "xyz/openbmc_project/Common/error.hpp"
 
+#include <CLI/CLI.hpp>
 #include <iostream>
+#include <phosphor-logging/elog-errors.hpp>
 
 int main(int argc, char** argv)
 {
-    openpower::boot::Control ctrl;
+    using namespace phosphor::logging;
+    using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+    CLI::App app{"OpenPOWER boot control"};
+
+    // Boot step major number
+    std::string major;
+    app.add_option("-m,--major", major, "Step Major Number")->required();
+
+    // Boot step minor number
+    std::string minor;
+    app.add_option("-i,--minor", minor, "Step Minor Number")->required();
+    CLI11_PARSE(app, argc, argv);
+
+    uint8_t major_number = 0;
+    uint8_t minor_number = 0;
     try
     {
-        ctrl.executeStep(0, 0);
+        major_number = std::stoi(major);
+        minor_number = std::stoi(minor);
+
+        openpower::boot::Control ctrl;
+        ctrl.executeStep(major_number, minor_number);
     }
     catch (std::exception& e)
     {
         std::cerr << "Error in executing the step " << e.what() << std::endl;
+        report<InternalFailure>();
         exit(-1);
     }
     exit(0);
