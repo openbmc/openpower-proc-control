1faefd32fcd1879e339302ea5fd026bf62bee2ac
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-09-18 05:29:35 -0500

    Initial commit for boot control application
    This is the skelton of the boot control app.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ia44ea8641bf9f09d2dc059dfffcbc5ebe8c71e00

---
+++ boot-control/boot_control.cpp
@@ -0,0 +1,32 @@
+#include "boot_control.hpp"
+
+#include <iostream>
+
+namespace open_power
+{
+namespace boot
+{
+int Control::executeHostStep(uint8_t stepMajor, uint8_t stepMinor)
+{
+    return 0;
+}
+
+int Control::executeBmcStep(uint8_t stepMajor, uint8_t stepMinor)
+{
+    return 0;
+}
+int Control::executeStep(uint8_t stepMajor, uint8_t stepMinor)
+{
+    int rc = -1;
+    if (stepMajor == 0)
+    {
+        rc = executeBmcStep(stepMajor, stepMinor);
+    }
+    else
+    {
+        rc = executeHostStep(stepMajor, stepMinor);
+    }
+    return rc;
+}
+} // namespace boot
+} // namespace open_power
