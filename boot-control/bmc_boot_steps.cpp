89db447ba20442197b5a17a411fffb87edfb2590
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-09-18 06:09:54 -0500

    Add support for BMC poweron istep
    Add support for BMC poweron istep by setting chassis on target
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ieddd45f679ad18a5a624bf2ae710f2d80eb281e0

--- boot-control/bmc_boot_steps.cpp
+++ boot-control/bmc_boot_steps.cpp
@@ -1,5 +1,10 @@
 #include "bmc_boot_steps.hpp"
 
+#include "util.hpp"
+
+#include <unistd.h>
+
+#include <chrono>
 #include <iostream>
 
 namespace open_power
@@ -8,6 +13,27 @@ namespace boot
 {
 namespace bmc_steps
 {
+
+int powerOn()
+{
+    // Poweron the chassis
+    util::chassisPowerOn();
+
+    auto sleepTime = std::chrono::seconds(5);
+    auto timeout = std::chrono::seconds(20);
+    auto now = std::chrono::system_clock::now();
+    while (std::chrono::system_clock::now() < (now + timeout))
+    {
+        if (util::isChassisOn())
+        {
+            return 0;
+        }
+        std::this_thread::sleep_for(sleepTime);
+        sleepTime = std::chrono::seconds(2);
+    }
+
+    return -1;
+}
 
 int StubbedStep()
 {
