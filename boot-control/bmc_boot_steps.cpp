c0a42e99851fd56a3bd2a0b0e968b6a66980dae3
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-09-18 06:09:54 -0500

    Add support for BMC poweron istep
    Add support for BMC poweron istep by setting chassis on target
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ieddd45f679ad18a5a624bf2ae710f2d80eb281e0

--- boot-control/bmc_boot_steps.cpp
+++ boot-control/bmc_boot_steps.cpp
@@ -1,5 +1,11 @@
 #include "bmc_boot_steps.hpp"
 
+#include "util.hpp"
+#include "xyz/openbmc_project/Common/error.hpp"
+
+#include <unistd.h>
+
+#include <chrono>
 #include <iostream>
 #include <phosphor-logging/elog-errors.hpp>
 
@@ -10,6 +16,30 @@ namespace boot
 namespace bmc_steps
 {
 using namespace phosphor::logging;
+using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+
+void powerOn()
+{
+    // Poweron the chassis
+    util::chassisPowerOn();
+
+    auto sleepTime = std::chrono::seconds(5);
+    auto timeout = std::chrono::seconds(60);
+    auto now = std::chrono::system_clock::now();
+    // TODO: change this to signal based wait for chassis poweron
+    while (std::chrono::system_clock::now() < (now + timeout))
+    {
+        if (util::isChassisOn())
+        {
+            return;
+        }
+        std::this_thread::sleep_for(sleepTime);
+        sleepTime = std::chrono::seconds(2);
+    }
+
+    log<level::ERR>("Timeout encountered while waiting for chassis on");
+    elog<InternalFailure>();
+}
 
 void stubbedStep()
 {
