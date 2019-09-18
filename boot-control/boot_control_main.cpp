39ab0abf908d7f5018e4361eda032fff8198363a
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-09-18 05:39:48 -0500

    Add Argument parser to openpower-boot-control.
    Add support to pass arguments to the app.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: If46531abde2fd9638d31dbe884ce78a5bf7273fc

--- boot-control/boot_control_main.cpp
+++ boot-control/boot_control_main.cpp
@@ -1,9 +1,13 @@
+#include "argument.hpp"
 #include "boot_control.hpp"
 
 int main(int argc, char** argv)
 {
     int rc = -1;
+    open_power::boot::util::optstruct opt;
+    open_power::boot::util::parseArguments(argc, argv, opt);
+
     open_power::boot::Control ctrl;
-    rc = ctrl.executeStep(0, 0);
+    rc = ctrl.executeStep(opt.start_major, opt.start_minor);
     return rc;
 }
