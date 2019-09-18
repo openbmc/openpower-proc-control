106a9037b9be530d094d46e5271191299da0f1b2
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-09-18 06:22:27 -0500

    Support for executing a range of isteps
    Support for execute range of major steps by specifying
    start and end steps.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Iafe0b5e2b8811a10ee6573fd93bc42cf4a5fe40a

--- boot-control/boot_control.cpp
+++ boot-control/boot_control.cpp
@@ -15,6 +15,9 @@ using namespace sdbusplus::xyz::openbmc_project::Common::Error;
 
 BMCStepList Control::bmcSteps = {{0, {{0, []() { bmc_steps::powerOn(); }}}}};
 
+MajorStepsList Control::majorSteps = {{0, {{0, "poweron"}}},
+                                      {1, {{0, "sbestub"}}}};
+
 void Control::executeBMCStep(uint8_t stepMajor, uint8_t stepMinor)
 {
     auto mstep = bmcSteps.find(stepMajor);
@@ -45,5 +48,42 @@ void Control::executeStep(uint8_t stepMajor, uint8_t stepMinor)
     }
 }
 
+void Control::executeRange(uint8_t startStep, uint8_t endStep)
+{
+    if (endStep < startStep)
+    {
+        log<level::ERR>("Start step cannot be grater than end step",
+                        entry("STARTSTEP=%d", startStep),
+                        entry("ENDSTEP=%d", endStep));
+        elog<InternalFailure>();
+    }
+
+    auto begin_step = majorSteps.find(startStep);
+    if (begin_step == majorSteps.end())
+    {
+        log<level::ERR>("Invalid start step", entry("STARTSTEP=%d", startStep));
+        elog<InternalFailure>();
+    }
+
+    auto end_step = majorSteps.find(endStep);
+    if (end_step == majorSteps.end())
+    {
+        log<level::ERR>("Invalid end step", entry("ENDSTEP=%d", endStep));
+        elog<InternalFailure>();
+    }
+    for (auto iter = begin_step; iter != majorSteps.end(); iter++)
+    {
+        for (auto& mStep : iter->second)
+        {
+            std::cout << "Executing:" << mStep.second << std::endl;
+            executeStep(iter->first, mStep.first);
+        }
+
+        if (endStep == iter->first)
+        {
+            break;
+        }
+    }
+}
 } // namespace boot
 } // namespace openpower
