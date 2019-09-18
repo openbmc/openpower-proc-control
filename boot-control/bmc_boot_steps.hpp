a77fa24ffab17e881b522d3c8d3ed35201cc6e84
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-09-18 05:56:48 -0500

    Base infrastructure to execute BMC istpes.
    Add stubs for step0 BMC steps.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ic98655a5fbf553a86f29746f7fc09c70ea23cdcc

---
+++ boot-control/bmc_boot_steps.hpp
@@ -0,0 +1,13 @@
+namespace openpower
+{
+namespace boot
+{
+namespace bmc_steps
+{
+
+/** @brief An place holder for stubbed steps
+ */
+void stubbedStep();
+} // namespace bmc_steps
+} // namespace boot
+} // namespace openpower
