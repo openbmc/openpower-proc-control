ca3b1cf3c2cb3206582eae7dfdbcd96b8d6b93de
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-09-18 05:56:48 -0500

    Base infrastructure to execute BMC istpes.
    Add support for BMC poweron istep
    Add support for BMC poweron istep by setting chassis on target
    Support for execute range of major steps by specifying
    start and end steps.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ic98655a5fbf553a86f29746f7fc09c70ea23cdcc

---
+++ boot-control/bmc_boot_steps.hpp
@@ -0,0 +1,14 @@
+#pragma once
+namespace openpower
+{
+namespace boot
+{
+namespace bmc_steps
+{
+
+/** @brief An place holder for stubbed steps
+ */
+void stubbedStep();
+} // namespace bmc_steps
+} // namespace boot
+} // namespace openpower
--- boot-control/boot_control_main.cpp
+++ boot-control/boot_control_main.cpp
@@ -3,7 +3,29 @@
 
 #include <CLI/CLI.hpp>
 #include <iostream>
+#include <optional>
 #include <phosphor-logging/elog-errors.hpp>
+
+using BootSeq = std::pair<uint8_t, uint8_t>;
+
+BootSeq getBootSeq(std::string steps)
+{
+    using namespace phosphor::logging;
+    using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+    BootSeq bs;
+    auto splitPos = steps.find("..");
+    if (splitPos == std::string::npos)
+    {
+        uint8_t number = std::stoi(steps);
+        bs = std::make_pair(number, number);
+    }
+    else
+    {
+        bs = std::make_pair(std::stoi(steps.substr(0, splitPos)),
+                            std::stoi(steps.substr(splitPos + 2)));
+    }
+    return bs;
+}
 
 int main(int argc, char** argv)
 {
@@ -11,23 +33,52 @@ int main(int argc, char** argv)
     using namespace sdbusplus::xyz::openbmc_project::Common::Error;
     CLI::App app{"OpenPOWER boot control"};
 
-    // Boot step major number
-    int major;
-    app.add_option("-m,--major", major, "Step Major Number")->required();
-
-    // Boot step minor number
-    int minor;
-    app.add_option("-i,--minor", minor, "Step Minor Number")->required();
-    CLI11_PARSE(app, argc, argv);
-
     try
     {
+        std::string normalOptions = "Option specify single boot step";
+        // Boot step major number
+        std::optional<int> major;
+        app.add_option("-m,--major", major, "Step Major Number")
+            ->group(normalOptions);
+
+        // Boot step minor number
+        std::optional<int> minor;
+        app.add_option("-i,--minor", minor, "Step Minor Number")
+            ->group(normalOptions);
+
+        std::string rangeOptions = "Option to specify range of boot steps";
+
+        std::optional<std::string> steps;
+        app.add_option(
+               "-s, --step", steps,
+               "Steps to be executed, range of steps to be separated by '..'")
+            ->group(rangeOptions);
+        CLI11_PARSE(app, argc, argv);
+
         openpower::boot::Control ctrl;
-        ctrl.executeStep(major, minor);
+
+        if ((minor) && (major))
+        {
+            ctrl.executeStep(*major, *minor);
+        }
+        else if (steps)
+        {
+            auto bs = getBootSeq(*steps);
+            ctrl.executeRange(bs.first, bs.second);
+        }
     }
-    catch (std::exception& e)
+    catch (const InternalFailure& e)
     {
-        std::cerr << "Error in executing the step " << e.what() << std::endl;
+        log<level::ERR>("Error in executing boot steps");
+        std::cerr << "Error in executing the boot steps " << e.what()
+                  << std::endl;
+        commit<InternalFailure>();
+        std::exit(EXIT_FAILURE);
+    }
+    catch (const std::exception& e)
+    {
+        log<level::ERR>("Error in executing boot steps ",
+                        entry("ERROR=%s", e.what()));
         report<InternalFailure>();
         std::exit(EXIT_FAILURE);
     }
