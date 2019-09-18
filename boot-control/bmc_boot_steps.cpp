896c24829b97974124ac89414b55749da7b43acf
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-09-18 05:56:48 -0500

    Base infrastructure to execute BMC istpes.
    Add stubs for step0 BMC steps.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ic98655a5fbf553a86f29746f7fc09c70ea23cdcc

---
+++ boot-control/bmc_boot_steps.cpp
@@ -0,0 +1,20 @@
+#include "bmc_boot_steps.hpp"
+
+#include <iostream>
+#include <phosphor-logging/elog-errors.hpp>
+
+namespace openpower
+{
+namespace boot
+{
+namespace bmc_steps
+{
+using namespace phosphor::logging;
+
+void stubbedStep()
+{
+    log<level::INFO>("Step is a stub");
+}
+} // namespace bmc_steps
+} // namespace boot
+} // namespace openpower
--- boot-control/boot_control.hpp
+++ boot-control/boot_control.hpp
@@ -1,10 +1,17 @@
 #pragma once
+
 #include <stdint.h>
+
+#include <functional>
+#include <map>
 
 namespace openpower
 {
 namespace boot
 {
+
+using BMCExecList = std::map<uint8_t, std::function<void(void)>>;
+using BMCStepList = std::map<uint8_t, BMCExecList>;
 
 /** @class Control
  *  @brief Implements boot step control
@@ -30,6 +37,8 @@ class Control
     void executeStep(uint8_t stepMajor, uint8_t stepMinor);
 
   private:
+    static BMCStepList bmcSteps;
+
     /** @brief Execute a boot step in BMC.
      *  @param[in] stepMajor - a Major boot step.
      *  @param[in] stepMinor - a Minor boot step or substep.
