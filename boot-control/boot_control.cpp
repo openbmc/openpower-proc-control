398a0a3503d03de2fc6a6619dd7902500f387e27
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-09-18 06:09:54 -0500

    Add support for BMC poweron istep
    Add support for BMC poweron istep by setting chassis on target
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ieddd45f679ad18a5a624bf2ae710f2d80eb281e0

--- boot-control/boot_control.cpp
+++ boot-control/boot_control.cpp
@@ -1,18 +1,40 @@
 #include "boot_control.hpp"
 
 #include "bmc_boot_steps.hpp"
+#include "xyz/openbmc_project/Common/error.hpp"
+
+#include <iostream>
+#include <phosphor-logging/elog-errors.hpp>
 
 namespace openpower
 {
 namespace boot
 {
+using namespace phosphor::logging;
+using namespace sdbusplus::xyz::openbmc_project::Common::Error;
 
-BMCStepList Control::bmcSteps = {
-    {0, {{0, []() { bmc_steps::stubbedStep(); }}}}};
+BMCStepList Control::bmcSteps = {{0, {{0, []() { bmc_steps::powerOn(); }}}}};
 
 void Control::executeBMCStep(uint8_t stepMajor, uint8_t stepMinor)
 {
-    return;
+    auto mstep = bmcSteps.find(stepMajor);
+    if (mstep == bmcSteps.end())
+    {
+        log<level::ERR>("Invalid BMC Major step",
+                        entry("MAJORSTEP=%d", stepMajor));
+        elog<InternalFailure>();
+    }
+
+    auto istep = mstep->second.find(stepMinor);
+    if (istep == mstep->second.end())
+    {
+        log<level::ERR>("Invalid BMC Minor step",
+                        entry("MINORSTEP=%d", stepMinor));
+        elog<InternalFailure>();
+    }
+
+    // Execute the BMC step
+    (istep->second)();
 }
 
 void Control::executeStep(uint8_t stepMajor, uint8_t stepMinor)
@@ -21,7 +43,6 @@ void Control::executeStep(uint8_t stepMajor, uint8_t stepMinor)
     {
         executeBMCStep(stepMajor, stepMinor);
     }
-    return;
 }
 
 } // namespace boot
