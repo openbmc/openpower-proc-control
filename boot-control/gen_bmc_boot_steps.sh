c973841bf2424e93fcb0d14d51b9184a8d2ce5bb
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-09-24 08:23:13 -0500

    Add the list of steps in json
    Add the support to read the list of boot steps from a
    json file.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I685cfbd4c2bd2bfca0dc584bac57939c4539692f

--- boot-control/gen_bmc_boot_steps.sh
+++ boot-control/gen_bmc_boot_steps.sh
@@ -7,7 +7,9 @@ if [[ $CHIP_VERSION == "p9" ]]
 then
   cat $DIR/boot-control/$CHIP_VERSION/bmc_boot_steps_p9.cpp > $DIR/boot-control/bmc_boot_steps.cpp
   cat $DIR/boot-control/$CHIP_VERSION/bmc_boot_steps_p9.hpp > $DIR/boot-control/bmc_boot_steps.hpp
+  cat $DIR/boot-control/$CHIP_VERSION/boot_steps_p9.json > $DIR/boot-control/boot_steps.json
 else # Default is p10
   cat $DIR/boot-control/$CHIP_VERSION/bmc_boot_steps_p10.cpp > $DIR/boot-control/bmc_boot_steps.cpp
   cat $DIR/boot-control/$CHIP_VERSION/bmc_boot_steps_p10.hpp > $DIR/boot-control/bmc_boot_steps.hpp
+  cat $DIR/boot-control/$CHIP_VERSION/boot_steps_p10.json > $DIR/boot-control/boot_steps.json
 fi
