5d71ddf4eb88d643c3fe4e5970f8d79600b96478
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-09-24 08:23:13 -0500

    Add the list of steps in json
    Add the support to read the list of boot steps from a
    json file.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: I685cfbd4c2bd2bfca0dc584bac57939c4539692f

--- boot-control/boot_control_main.cpp
+++ boot-control/boot_control_main.cpp
@@ -16,6 +16,7 @@ int main(int argc, char** argv)
     {
         return ctrl.executeStep(opt.start_major, opt.start_minor);
     }
+
     // else execute the range
     return ctrl.executeRange(opt.start_major, opt.end_major);
 }
