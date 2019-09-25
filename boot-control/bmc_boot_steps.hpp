a9405660a2d93476d9c5bd2995aff4f67d2d2748
Ramesh Iyyar <rameshi1@in.ibm.com>
2019-09-25 11:06:29 -0500

    Enabled p10 step 0 hardware procedure
    Enabled p10 and startSbe sub steps in step 0 by using pHAL
    infrastructure
    
    Change-Id: I044ef43a7544d71c82498f388a351e9b3d7a95cf
    Signed-off-by: Ramesh Iyyar <rameshi1@in.ibm.com>

--- boot-control/bmc_boot_steps.hpp
+++ boot-control/bmc_boot_steps.hpp
@@ -6,6 +6,10 @@ namespace boot
 namespace bmc_steps
 {
 
+/** @brief Execute Start SBE hardware procedure
+ */
+void startSbe();
+
 /** @brief An place holder for stubbed steps
  */
 void stubbedStep();
--- boot-control/boot_control.cpp
+++ boot-control/boot_control.cpp
@@ -13,10 +13,11 @@ namespace boot
 using namespace phosphor::logging;
 using namespace sdbusplus::xyz::openbmc_project::Common::Error;
 
-BMCStepList Control::bmcSteps = {
-    {0, {{0, []() { bmc_steps::stubbedStep(); }}}}};
+BMCStepList Control::bmcSteps = {{0,
+                                  {{0, []() { bmc_steps::stubbedStep(); }},
+                                   {7, []() { bmc_steps::startSbe(); }}}}};
 
-MajorStepsList Control::majorSteps = {{0, {{0, "sbestub"}}},
+MajorStepsList Control::majorSteps = {{0, {{0, "sbestub"}, {7, "startsbe"}}},
                                       {1, {{0, "sbestub"}}}};
 
 void Control::executeBMCStep(uint8_t stepMajor, uint8_t stepMinor)
