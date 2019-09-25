a18545feef6152f3d0aaa92f87b72051d7d2b1f2
Ramesh Iyyar <rameshi1@in.ibm.com>
2019-09-25 11:06:29 -0500

    Enabled p10 step 0 hardware procedure
    Enabled p10 and startSbe sub steps in step 0 by using pHAL
    infrastructure
    
    Change-Id: I044ef43a7544d71c82498f388a351e9b3d7a95cf
    Signed-off-by: Ramesh Iyyar <rameshi1@in.ibm.com>

---
+++ boot-control/pdbg.hpp
@@ -0,0 +1,20 @@
+#pragma once
+namespace openpower
+{
+namespace boot
+{
+namespace pdbg
+{
+/**
+ * @brief Load attributes atdb file
+ */
+void initatdb();
+
+/**
+ * @brief Initialize targets
+ */
+void initTargets();
+
+} // namespace pdbg
+} // namespace boot
+} // namespace openpower
