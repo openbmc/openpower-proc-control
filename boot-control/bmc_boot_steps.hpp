5ac238ba1cb24084882a39a19e21f32024f9426d
Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
2019-09-28 15:12:17 -0500

    Add support for SBE steps.
    Add support to trigger SBE boot steps by calling istep
    chip-op. This will be changed once the HWP is available
    for calling both SBE and hostboot boot steps.
    
    Signed-off-by: Dhruvaraj Subhashchandran <dhruvaraj@in.ibm.com>
    Change-Id: Ic51a94d823950963568ae8aba0c744e8e53fb9b6

---
+++ boot-control/bmc_boot_steps.hpp
@@ -0,0 +1,20 @@
+namespace openpower
+{
+namespace boot
+{
+namespace bmc_steps
+{
+/** @brief Apply power to the chassis.
+ */
+void powerOn();
+
+/** @brief Execute Start SBE hardware procedure
+ */
+void startSbe();
+
+/** @brief An place holder for stubbed steps
+ */
+void stubbedStep();
+} // namespace bmc_steps
+} // namespace boot
+} // namespace openpower
