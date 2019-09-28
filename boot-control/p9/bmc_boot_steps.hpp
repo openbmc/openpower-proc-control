624a8758119c1ba6e72ebb5ba2cde129fb920dde
Ramesh Iyyar <rameshi1@in.ibm.com>
2019-09-28 00:24:17 -0500

    Enabled p9 step 0 hardware procedure
    Enabled p9 sbeconfigupdate and startcbs sub steps in step 0 by using
    pHAL infrastructure
    
    From this commit, the openpower-boot-control application will support p9
    and p10 HWP by using CHIP_VERSION configure variable and same variable
    can pass from recipe with chip version.
        For Ex:
            CHIP_VERSION = p9
            or
            CHIP_VERSION = p10
    
    Change-Id: Ia5fae6e40b76f030782063b261cfbd613bbefc8b
    Signed-off-by: Ramesh Iyyar <rameshi1@in.ibm.com>

---
+++ boot-control/p9/bmc_boot_steps.hpp
@@ -0,0 +1,21 @@
+namespace openpower
+{
+namespace boot
+{
+namespace bmc_steps
+{
+
+/** @brief Execute Start SBE hardware procedure
+ */
+void startSbe();
+
+/** @brief Execute SbeConfigUpdate hardware procedure
+ */
+void sbeConfigUpdate();
+
+/** @brief An place holder for stubbed steps
+ */
+void stubbedStep();
+} // namespace bmc_steps
+} // namespace boot
+} // namespace openpower
