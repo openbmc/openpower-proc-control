2fa7bd6a769c548729832fa7723ed1a32f8db160
Ramesh Iyyar <rameshi1@in.ibm.com>
2019-09-28 00:24:17 -0500

    Enabled p9 step 0 hardware procedure
    Enabled p9 sbeconfigupdate and startcbs sub steps in step 0 by using
    pHAL infrastructure
    
    From this commit, the openpower-boot-control application will support p9
    and p10 HWP by using CHIP_VERSION configure variable and same variable
    can pass from recipe with chip version.
        For Ex:
            CHIP_VERSION = p9
            or
            CHIP_VERSION = p10
    
    Change-Id: Ia5fae6e40b76f030782063b261cfbd613bbefc8b
    Signed-off-by: Ramesh Iyyar <rameshi1@in.ibm.com>

---
+++ boot-control/p9/bmc_boot_steps_p9.cpp
@@ -0,0 +1,104 @@
+extern "C" {
+#include <libpdbg.h>
+}
+
+#include "bmc_boot_steps.hpp"
+#include "pdbg_wrapper.hpp"
+#include "util.hpp"
+#include "xyz/openbmc_project/Common/error.hpp"
+
+#include <p9_nv_ref_clk_enable.H>
+#include <p9_setup_sbe_config.H>
+#include <p9_start_cbs.H>
+#include <unistd.h>
+
+#include <chrono>
+#include <iostream>
+#include <phosphor-logging/elog-errors.hpp>
+
+namespace openpower
+{
+namespace boot
+{
+namespace bmc_steps
+{
+using namespace phosphor::logging;
+using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+
+void powerOn()
+{
+    // Poweron the chassis
+    util::chassisPowerOn();
+
+    auto sleepTime = std::chrono::seconds(5);
+    auto timeout = std::chrono::seconds(60);
+    auto now = std::chrono::system_clock::now();
+    // TODO: change this to signal based wait for chassis poweron
+    while (std::chrono::system_clock::now() < (now + timeout))
+    {
+        if (util::isChassisOn())
+        {
+            // Init targets only if chassin is on
+            util::pdbg::initTargets();
+            return;
+        }
+        std::this_thread::sleep_for(sleepTime);
+        sleepTime = std::chrono::seconds(2);
+    }
+
+    log<level::ERR>("Timeout encountered while waiting for chassis on");
+    elog<InternalFailure>();
+}
+
+void sbeConfigUpdate()
+{
+    struct pdbg_target* pib;
+
+    // TODO - Curently no attribute support available to chcek master
+    // processor. First processor in the list is the master processor.
+    // This should change, once master processor attribute is enabled
+    pdbg_for_each_class_target("pib", pib)
+    {
+        int rc = -1;
+        if ((rc = p9_setup_sbe_config(pib)) != 0)
+        {
+            log<level::ERR>("sbeConfigUpdate is failed",
+                            entry("RETURNCODE=%d", rc));
+            elog<InternalFailure>();
+        }
+        return;
+    }
+}
+
+void startSbe()
+{
+    int rc = -1;
+    struct pdbg_target* pib;
+
+    // TODO - Curently no attribute support available to chcek master
+    // processor. First processor in the list is the master processor.
+    // This should change, once master processor attribute is enabled
+    pdbg_for_each_class_target("pib", pib)
+    {
+        if ((rc = p9_start_cbs(pib, true)) != 0)
+        {
+            log<level::ERR>("startSbe is failed", entry("RETURNCODE=%d", rc));
+            elog<InternalFailure>();
+        }
+        if ((rc = p9_nv_ref_clk_enable(pib)) != 0)
+        {
+            log<level::ERR>("nvRefClkEnable is failed",
+                            entry("RETURNCODE=%d", rc));
+            elog<InternalFailure>();
+        }
+        return;
+    }
+}
+
+void stubbedStep()
+{
+    log<level::INFO>("Step is a stub");
+}
+} // namespace bmc_steps
+} // namespace boot
+} // namespace openpower
