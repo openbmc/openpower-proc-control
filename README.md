22a6cc84f34ecb1fd2b9a0381092884436076f87
Ramesh Iyyar <rameshi1@in.ibm.com>
2019-10-17 13:39:10 -0500

    Enabled pHAL infrastructure to boot the host
    In this commit, libipl is used from pHAL to
    boot the system
    
    The existing CHIPS configure variable is used
    to pass phal directory name to boot the system
    along with --enable-phal.
    
    Ex:
      ./configure --enable-phal CHIPS="phal openfsi"
    
    Tested by:
      1. ./configure
      2. ./configure --enable-phal CHIPS="phal openfsi"
    
    Change-Id: I57ff64595a757041ee60ccb420092975b064462d
    Signed-off-by: Ramesh Iyyar <rameshi1@in.ibm.com>

--- README.md
+++ README.md
@@ -8,5 +8,15 @@ To build this package, do the following steps:
     2. ./configure ${CONFIGURE_FLAGS}
     3. make
 
+To build with phal feature:
+    1. ./bootstrap.sh
+    2. i)  To build for p9 target host
+             ./configure ${CONFIGURE_FLAGS} --enable-phal CHIPS="p9 openfsi"
+       ii) To build for p10 target host
+             ./configure ${CONFIGURE_FLAGS} --enable-phal CHIPS="p10 openfsi"
+    3. make
+
+    Note: At present pHAL is supporting p9 and p10 target processor.
+
 To clean the repository run `./bootstrap.sh clean`.
 ```
---
+++ procedures/phal/start_host.cpp
@@ -0,0 +1,41 @@
+extern "C" {
+#include <libipl.h>
+}
+
+#include "xyz/openbmc_project/Common/error.hpp"
+
+#include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/log.hpp>
+#include <registration.hpp>
+namespace openpower
+{
+namespace phal
+{
+
+using namespace phosphor::logging;
+using namespace sdbusplus::xyz::openbmc_project::Common::Error;
+
+/**
+ * @brief Starts the self boot engine on POWER processor position 0
+ *        to kick off a boot.
+ * @return void
+ */
+void startHost()
+{
+    if (ipl_init() != 0)
+    {
+        log<level::ERR>("ipl_init failed to load atdb");
+        elog<InternalFailure>();
+    }
+
+    if (ipl_run_major(0) > 0)
+    {
+        log<level::ERR>("step 0 failed to start the host");
+        elog<InternalFailure>();
+    }
+}
+
+REGISTER_PROCEDURE("startHost", startHost);
+
+} // namespace phal
+} // namespace openpower
