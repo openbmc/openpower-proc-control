e12b736ece6097c01fdca0429b1b29d0cd268a84
Marri Devender Rao <devenrao@in.ibm.com>
2020-01-06 03:45:11 -0600

    Parse return code object and create PEL on failure
    Made changes to parse the fapi2::ReturnCode returned from
    ipl library and create PEL (platform entity log) on failure.
    
    Signed-off-by: Marri Devender Rao <devenrao@in.ibm.com>
    Change-Id: I06491f09041edcd16d8db5890c74a4db8d08e920

--- procedures/phal/start_host.cpp
+++ procedures/phal/start_host.cpp
@@ -1,12 +1,12 @@
-extern "C" {
-#include <libipl.h>
-}
+#include <libipl.H>
+#include <return_code.H>
 
-#include "xyz/openbmc_project/Common/error.hpp"
-
+#include <phalerror/create_pel.hpp>
 #include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/log.hpp>
 #include <registration.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+
 namespace openpower
 {
 namespace phal
@@ -25,15 +25,17 @@ void startHost()
     if (ipl_init() != 0)
     {
         log<level::ERR>("ipl_init failed");
-        // TODO ibm-openbmc#1470
-        elog<InternalFailure>();
+        openpower::pel::createBootInitErrorPEL();
+        throw std::runtime_error("Error returned by call to ipl_init");
     }
 
-    if (ipl_run_major(0) > 0)
+    fapi2::ReturnCode rc = ipl_run_major(0);
+    if (rc != fapi2::FAPI2_RC_SUCCESS &&
+        rc != fapi2::FAPI2_RC_PHAL_NOT_SUPPORTED)
     {
         log<level::ERR>("step 0 failed to start the host");
-        // TODO ibm-openbmc#1470
-        elog<InternalFailure>();
+        openpower::pel::createFAPIReturnCodeErrorPEL(rc);
+        throw std::runtime_error("Error returned by call to ipl_run_major_ret");
     }
 }
 
