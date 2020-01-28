d0c9aeefe04e777c3360b40cce968ccb82aa1aa2
Eddie James <eajames@linux.ibm.com>
2020-01-28 10:25:13 -0600

    Add procedure and service file to set the SPI mux on P10
    This procedure will be called before powering on in order to set the mux
    for accessing host EEPROMs from the BMC through the FSI SPI masters.
    
    Signed-off-by: Eddie James <eajames@linux.ibm.com>
    Change-Id: I238d3bc449c5924740cf4c2e69d794605a6ae916

---
+++ p10_cfam.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace openpower
+{
+namespace cfam
+{
+namespace p10
+{
+
+static constexpr uint16_t P10_ROOT_CTRL8 = 0x2818;
+
+} // namespace p10
+} // namespace cfam
+} // namespace openpower
