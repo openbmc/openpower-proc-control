dacc85521e293e0a0093e87efa377ccc7413039c
Ramesh Iyyar <rameshi1@in.ibm.com>
2020-04-22 01:32:37 -0500

    procedures/phal/start_host: Initialized libpdbg and libekb
    Currently ipl_init is initialized libpdbg and libekb but
    as per recent design changes, phal expecting all phal library
    initialization should be happen in application rather than
    doing via ipl_init. so, same support added in this patch.
    
    Signed-off-by: Ramesh Iyyar <rameshi1@in.ibm.com>
    Change-Id: I38026082cfa2fb21e042b50ddb7c627956a317be

--- procedures/phal/start_host.cpp
+++ procedures/phal/start_host.cpp
@@ -1,5 +1,10 @@
+extern "C" {
+#include <libpdbg.h>
+}
+
 #include "phalerror/phal_error.hpp"
 
+#include <libekb.H>
 #include <libipl.H>
 
 #include <phosphor-logging/log.hpp>
@@ -20,6 +25,20 @@ void startHost()
 {
     // add callback methods for debug traces and for boot failures
     openpower::pel::addBootErrorCallbacks();
+
+    // callback method will be called upon failure which will create the PEL
+    if (!pdbg_targets_init(NULL))
+    {
+        log<level::ERR>("pdbg_targets_init failed");
+        throw std::runtime_error("pdbg target initialization failed");
+    }
+
+    // callback method will be called upon failure which will create the PEL
+    if (libekb_init())
+    {
+        log<level::ERR>("libekb_init failed");
+        throw std::runtime_error("libekb initialization failed");
+    }
 
     // callback method will be called upon failure which will create the PEL
     if (ipl_init(IPL_AUTOBOOT) != 0)
