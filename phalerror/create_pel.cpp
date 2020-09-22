ef767eea87b785931b473926b2f1136b22c5ce38
Ramesh Iyyar <rameshi1@in.ibm.com>
2020-09-22 02:46:51 -0500

    phal: Added HW callout info into PEL
    - If any hardware procedure (hwp) failed during execution of ipling then
      that particular hwp will request the required hardware's (which are defined
      error xml of respective hwp) need to be callout. So, this patch will create
      PEL records based on given FFDC (First Failure Data Collection) by phal
      during failure.
    
    - Also, this patch will deconfigure (marking hardware as non-functional
      i.e update HWAS_STATE attribute with respective bitfield value in POWER CEC
      device tree) required hardwares which are given as part of phal ffdc data.
    
    - Used "libfmt" to format trace with field value instead adding as entry
      in journal so, while debugging it will be easy instead looking each entry.
    
    TestedBy:
    
    "User Data 1": {
        "Section Version": "1",
        "Sub-section type": "1",
        "Created by": "0x2000",
        "HWP_CDG_TGT_01_CO_PRIORITY": "MEDIUM",
        "HWP_CDG_TGT_01_CO_REQ": "true",
        "HWP_CDG_TGT_01_DECONF_REQ": "false",
        "HWP_CDG_TGT_01_GUARD_REQ": "false",
        "HWP_CDG_TGT_01_GUARD_TYPE": "GARD_Fatal",
        "HWP_CDG_TGT_01_LOC_CODE": "Ufcs-P0-C15",
        "HWP_CDG_TGT_01_PHYS_PATH": "physical:sys-0/node-0/proc-0",
        "HWP_FFDC_ATTR_CP_REFCLOCK_SELECT_VALUE": "00",
        "HWP_FFDC_RCS_CLOCK_TEST_IN": "01",
        "HWP_FFDC_READ_SNS1LTH": "0c000000",
        "HWP_HW_CO_01_CLK_POS": "0",
        "HWP_HW_CO_01_HW_ID": "PROC_REF_CLOCK",
        "HWP_HW_CO_01_LOC_CODE": "Ufcs-P0-C15",
        "HWP_HW_CO_01_PHYS_PATH": "physical:sys-0/node-0/proc-0",
        "HWP_HW_CO_01_PRIORITY": "HIGH",
        "HWP_RC": "RC_CLOCK_TEST_OUT_RCS_ERR",
        "HWP_RC_DESC": "One or more processor reference clocks not toggling",
        "LOG000 2020-09-22 04:12:58": "Running p10_clock_test HWP on processor 0\n",
        "LOG001 2020-09-22 04:12:58": "inf: p10_clock_test:60
    p10_clock_test: Entering ...\n",
        "LOG002 2020-09-22 04:12:58": "inf: p10_clock_test_latches:106
    p10_clock_test_latches: Entering
    ...\n",
        "LOG003 2020-09-22 04:12:58": "err: CLOCK_TEST_OUT_RCS_ERR:4036
    One or more processor reference clocks not toggling\n",
        "LOG004 2020-09-22 04:12:58": "err: _setHwpError:120 _setHwpError:
    Creating HWP error 0x927e1c\n",
        "LOG005 2020-09-22 04:12:58": "inf: addErrorInfo:344
    addErrorInfo: Adding cdg (1:0:0), pri: 1, gtyp: 227\n",
        "LOG006 2020-09-22 04:12:58": "err: p10_clock_test_latches:130 Clock A is bad\n",
        "LOG007 2020-09-22 04:12:58": "err: p10_clock_test:76
    p10_clock_test_latches(i_target_chip, true) failed.\n",
        "LOG008 2020-09-22 04:12:58": "HWP clock_test failed on proc 0, rc=2124995320\n",
        "_PID": "1209"
    },
    "User Data 2": {
        "Section Version": "1",
        "Sub-section type": "1",
        "Created by": "0x2000",
        "Data": [
            {
                "LocationCode": "Ufcs-P0-C15",
                "MRUs": [
                    {
                        "ID": 65536,
                        "Priority": "H"
                    }
                ],
                "Priority": "H"
            },
            {
                "Deconfigured": false,
                "Guarded": false,
                "LocationCode": "Ufcs-P0-C15",
                "MRUs": [
                    {
                        "ID": 65536,
                        "Priority": "M"
                    }
                ],
                "Priority": "M"
            }
        ]
    }
    
    Signed-off-by: Ramesh Iyyar <rameshi1@in.ibm.com>
    Change-Id: I4462c46747598166b085990e1d82cb2998b9e06b

--- phalerror/create_pel.cpp
+++ phalerror/create_pel.cpp
@@ -1,11 +1,23 @@
 #include "create_pel.hpp"
 
+#include <fcntl.h>
+#include <fmt/format.h>
 #include <libekb.H>
 #include <unistd.h>
 
+#include <cerrno>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <map>
 #include <phosphor-logging/elog.hpp>
+#include <stdexcept>
+#include <string>
+#include <tuple>
+#include <vector>
+#include <xyz/openbmc_project/Logging/Create/server.hpp>
 #include <xyz/openbmc_project/Logging/Entry/server.hpp>
+
 namespace openpower
 {
 using namespace phosphor::logging;
@@ -47,7 +59,7 @@ std::string getService(sdbusplus::bus::bus& bus, const std::string& objectPath,
 
 namespace pel
 {
-void createBootErrorPEL(const FFDCData& ffdcData)
+void createBootErrorPEL(const FFDCData& ffdcData, const json& calloutData)
 {
     constexpr auto loggingObjectPath = "/xyz/openbmc_project/logging";
     constexpr auto loggingInterface = "xyz.openbmc_project.Logging.Create";
@@ -59,19 +71,34 @@ void createBootErrorPEL(const FFDCData& ffdcData)
     {
         additionalData.emplace(data);
     }
+
     try
     {
+        FFDCFile ffdcFile(calloutData);
+
+        std::vector<std::tuple<sdbusplus::xyz::openbmc_project::Logging::
+                                   server::Create::FFDCFormat,
+                               uint8_t, uint8_t, sdbusplus::message::unix_fd>>
+            pelCalloutInfo;
+
+        pelCalloutInfo.push_back(
+            std::make_tuple(sdbusplus::xyz::openbmc_project::Logging::server::
+                                Create::FFDCFormat::JSON,
+                            static_cast<uint8_t>(0xCA),
+                            static_cast<uint8_t>(0x01), ffdcFile.getFileFD()));
+
         static constexpr auto bootErrorMessage =
             "org.open_power.PHAL.Error.Boot";
         std::string service =
             util::getService(bus, loggingObjectPath, loggingInterface);
-        auto method = bus.new_method_call(service.c_str(), loggingObjectPath,
-                                          loggingInterface, "Create");
+        auto method =
+            bus.new_method_call(service.c_str(), loggingObjectPath,
+                                loggingInterface, "CreateWithFFDCFiles");
         auto level =
             sdbusplus::xyz::openbmc_project::Logging::server::convertForMessage(
                 sdbusplus::xyz::openbmc_project::Logging::server::Entry::Level::
                     Error);
-        method.append(bootErrorMessage, level, additionalData);
+        method.append(bootErrorMessage, level, additionalData, pelCalloutInfo);
         auto resp = bus.call(method);
     }
     catch (const sdbusplus::exception::SdBusError& e)
@@ -84,6 +111,93 @@ void createBootErrorPEL(const FFDCData& ffdcData)
         throw std::runtime_error(
             "Error in invoking D-Bus logging create interface");
     }
+    catch (std::exception& e)
+    {
+        throw e;
+    }
 }
+
+FFDCFile::FFDCFile(const json& pHALCalloutData) :
+    calloutData(pHALCalloutData.dump()),
+    calloutFile("/tmp/phalPELCalloutsJson.XXXXXX"), fileFD(-1)
+{
+    prepareFFDCFile();
+}
+
+FFDCFile::~FFDCFile()
+{
+    removeCalloutFile();
+}
+
+int FFDCFile::getFileFD() const
+{
+    return fileFD;
+}
+
+void FFDCFile::prepareFFDCFile()
+{
+    createCalloutFile();
+    writeCalloutData();
+    setCalloutFileSeekPos();
+}
+
+void FFDCFile::createCalloutFile()
+{
+    fileFD = mkostemp(const_cast<char*>(calloutFile.c_str()), O_RDWR);
+
+    if (fileFD == -1)
+    {
+        log<level::ERR>(fmt::format("Failed to create phalPELCallouts "
+                                    "file({}), errorno({}) and errormsg({})",
+                                    calloutFile, errno, strerror(errno))
+                            .c_str());
+        throw std::runtime_error("Failed to create phalPELCallouts file");
+    }
+}
+
+void FFDCFile::writeCalloutData()
+{
+    ssize_t rc = write(fileFD, calloutData.c_str(), calloutData.size());
+
+    if (rc == -1)
+    {
+        log<level::ERR>(fmt::format("Failed to write phaPELCallout info "
+                                    "in file({}), errorno({}), errormsg({})",
+                                    calloutFile, errno, strerror(errno))
+                            .c_str());
+        throw std::runtime_error("Failed to write phalPELCallouts info");
+    }
+    else if (rc != static_cast<ssize_t>(calloutData.size()))
+    {
+        log<level::WARNING>(fmt::format("Could not write all phal callout "
+                                        "info in file({}), written byte({}) "
+                                        "and total byte({})",
+                                        calloutFile, rc, calloutData.size())
+                                .c_str());
+    }
+}
+
+void FFDCFile::setCalloutFileSeekPos()
+{
+    int rc = lseek(fileFD, 0, SEEK_SET);
+
+    if (rc == -1)
+    {
+        log<level::ERR>(fmt::format("Failed to set SEEK_SET for "
+                                    "phalPELCallouts in file({}), errorno({}) "
+                                    "and errormsg({})",
+                                    calloutFile, errno, strerror(errno))
+                            .c_str());
+        throw std::runtime_error(
+            "Failed to set SEEK_SET for phalPELCallouts file");
+    }
+}
+
+void FFDCFile::removeCalloutFile()
+{
+    close(fileFD);
+    std::remove(calloutFile.c_str());
+}
+
 } // namespace pel
 } // namespace openpower
