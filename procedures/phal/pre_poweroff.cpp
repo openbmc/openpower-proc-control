882412218e3ecbc87ff0c5f073e63f2f5c22154a
Chirag Sharma <chirshar@in.ibm.com>
2020-12-05 23:17:41 -0600

    Adding service op-pre-poweroff
    Changes:
    -Adding new service which will be called during
    chassis poweroff.
    
    Signed-off-by: Chirag Sharma <chirshar@in.ibm.com>
    Change-Id: Ic533433c4771216e5681b61cccf154f7ed029457

---
+++ procedures/phal/pre_poweroff.cpp
@@ -0,0 +1,33 @@
+#include "phalerror/phal_error.hpp"
+#include "procedures/phal/common_utils.hpp"
+#include "registration.hpp"
+
+#include <libekb.H>
+
+#include <phosphor-logging/log.hpp>
+
+namespace openpower
+{
+namespace phal
+{
+
+using namespace phosphor::logging;
+
+void prePoweroff(void)
+{
+    int rc;
+
+    phal_init();
+
+    rc = ipl_pre_poweroff();
+    if (rc)
+    {
+        log<level::ERR>("pre_poweroff failed");
+        throw std::runtime_error("pre_poweroff failed");
+    }
+}
+
+REGISTER_PROCEDURE("prePoweroff", prePoweroff)
+
+} // namespace phal
+} // namespace openpower
