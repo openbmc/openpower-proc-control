66fd53632af7812783ae701c873f220141b9d360
Chirag Sharma <chirshar@in.ibm.com>
2020-12-05 23:17:41 -0600

    Adding service proc-pre-poweroff
    Changes:
    -Adding new service which will be called during
    chassis poweroff.
    
    Test:
    -Did poweroff to check, if the service file
    is getting picked up or not and new added
    procedure is getting called.
    
    Signed-off-by: Chirag Sharma <chirshar@in.ibm.com>
    Change-Id: Ic533433c4771216e5681b61cccf154f7ed029457

---
+++ procedures/phal/common_utils.cpp
@@ -0,0 +1,52 @@
+extern "C"
+{
+#include <libpdbg.h>
+}
+
+#include "phalerror/phal_error.hpp"
+#include "procedures/phal/common_utils.hpp"
+
+#include <libekb.H>
+
+#include <phosphor-logging/log.hpp>
+
+namespace openpower
+{
+namespace phal
+{
+
+using namespace phosphor::logging;
+
+void phal_init(enum ipl_mode mode)
+{
+    // add callback methods for debug traces and for boot failures
+    openpower::pel::addBootErrorCallbacks();
+
+    if (!pdbg_targets_init(NULL))
+    {
+        log<level::ERR>("pdbg_targets_init failed");
+        openpower::pel::detail::processBootErrorCallback(false);
+        throw std::runtime_error("pdbg target initialization failed");
+    }
+    // To clear trace if success
+    openpower::pel::detail::processBootErrorCallback(true);
+
+    if (libekb_init())
+    {
+        log<level::ERR>("libekb_init failed");
+        openpower::pel::detail::processBootErrorCallback(false);
+        throw std::runtime_error("libekb initialization failed");
+    }
+    // To clear trace if success
+    openpower::pel::detail::processBootErrorCallback(true);
+
+    if (ipl_init(mode) != 0)
+    {
+        log<level::ERR>("ipl_init failed");
+        openpower::pel::detail::processBootErrorCallback(false);
+        throw std::runtime_error("Boot initialization failed");
+    }
+}
+
+} // namespace phal
+} // namespace openpower
