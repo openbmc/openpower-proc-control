b0f7b2dcf6a60a5229169903608da8867b646ff8
Jayanth Othayoth <ojayanth@in.ibm.com>
2021-06-08 01:33:40 -0500

    Added openpower util file support
    Created common file to keep all the common util functions.
    
    In this commit
     - added getService function to get d-bus service for given
       interface.
     - updated create_pel.cpp to start using common util interface.
    
    Tested: Manually verified.
    
    Signed-off-by: Jayanth Othayoth <ojayanth@in.ibm.com>
    Change-Id: I39a642710c0476e81fae020eb3b4216ca8ce9ad5

--- phalerror/create_pel.hpp
+++ phalerror/create_pel.hpp
@@ -1,26 +1,11 @@
 #pragma once
 
 #include <nlohmann/json.hpp>
-#include <sdbusplus/bus.hpp>
 
 #include <string>
 #include <vector>
 namespace openpower
 {
-namespace util
-{
-/**
- * Get D-Bus service name for the specified object and interface
- *
- * @param[in] bus - sdbusplus D-Bus to attach to
- * @param[in] objectPath - D-Bus object path
- * @param[in] interface - D-Bus interface name
- *
- * @return service name on success and exception on failure
- */
-std::string getService(sdbusplus::bus::bus& bus, const std::string& objectPath,
-                       const std::string& interface);
-} // namespace util
 namespace pel
 {
 using FFDCData = std::vector<std::pair<std::string, std::string>>;
