5748d7afcb4dd327ef896db320d6b90a36e4c927
Andrew Geissler <geissonator@yahoo.com>
2021-06-10 10:53:05 -0500

    master-proc: move function to common utils
    This is a useful function which can be utilized in upcoming work so move
    to the common utilities
    
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>
    Change-Id: I87873b4dbedbeef0a18474eba39798fde298ea36

--- procedures/phal/common_utils.hpp
+++ procedures/phal/common_utils.hpp
@@ -2,6 +2,11 @@
 
 #include <libipl.H>
 
+extern "C"
+{
+#include <libpdbg.h>
+}
+
 namespace openpower
 {
 namespace phal
@@ -16,6 +21,15 @@ namespace phal
  *
  */
 void phal_init(enum ipl_mode mode = IPL_AUTOBOOT);
+
+/**
+ *  @brief  Check if master processor or not
+ *
+ *  * @param[in] procTarget - Target to check if master or not
+ *
+ *  @return True/False
+ */
+bool isMasterProc(struct pdbg_target* procTarget);
 
 } // namespace phal
 } // namespace openpower
