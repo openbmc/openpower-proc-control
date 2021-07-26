4ab4055d46f38b7aa7041aa5a1e268d3d0c67323
Jayanth Othayoth <ojayanth@in.ibm.com>
2021-07-26 06:16:44 -0500

    PHAL: export devtree attribute support
    Added function to export devtree data based on the
    filter file using attributes tool based infrastructure.
    Data is stored in pre defined location in BMC to restore
    in the reboot path.
    
    Tested: verified devtree data exported manually.
    
    Signed-off-by: Jayanth Othayoth <ojayanth@in.ibm.com>
    Change-Id: I9c708483708f443a910e272615ec4c66b31e2603

--- extensions/phal/fw_update_watch.cpp
+++ extensions/phal/fw_update_watch.cpp
@@ -1,9 +1,21 @@
+#include "config.h"
+
 #include "fw_update_watch.hpp"
 
-#include <fmt/format.h>
+#include "common_utils.hpp"
+#include "create_pel.hpp"
+#include "pdbg_utils.hpp"
 
+#include <fmt/format.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/elog.hpp>
 #include <sdbusplus/exception.hpp>
+
+#include <filesystem>
 
 namespace openpower
 {
@@ -12,6 +24,8 @@ namespace phal
 namespace fwupdate
 {
 using namespace phosphor::logging;
+namespace fs = std::filesystem;
+
 using Message = std::string;
 using Attributes = std::variant<Message>;
 using AttributeName = std::string;
@@ -72,15 +86,88 @@ void Watch::fwIntfAddedCallback(sdbusplus::message::message& msg)
         // Device tree data collection is required only for the first trigger
         setSoftwareUpdateProgress(true);
 
-        // Colect device tree data
-        openpower::phal::fwupdate::exportDevtree();
+        try
+        {
+            // Colect device tree data
+            openpower::phal::fwupdate::exportDevtree();
+        }
+        catch (fs::filesystem_error& e)
+        {
+            log<level::ERR>(
+                fmt::format("Filesystem error reported Error:({})", e.what())
+                    .c_str());
+            throw std::runtime_error(e.what());
+        }
+
+        log<level::INFO>("Successfully exported devtree attribute data");
     }
 
     return;
 }
 
 void exportDevtree()
-{}
+{
+    // Check devtree export filter file is present
+    auto path = fs::path(DEVTREE_EXPORT_FILTER_FILE);
+    if (!fs::exists(path))
+    {
+        log<level::ERR>(
+            fmt::format("devtree export filter file is not available: ({})",
+                        DEVTREE_EXPORT_FILTER_FILE)
+                .c_str());
+        openpower::pel::createPEL("org.open_power.PHAL.Error.devtreeBackup");
+        return;
+    }
+
+    // delete export data file if present
+    auto expFile = fs::path(DEVTREE_EXP_FILE);
+    if (fs::exists(expFile))
+    {
+        fs::remove_all(expFile);
+    }
+    else
+    {
+        // create directory
+        fs::create_directory(expFile.parent_path());
+    }
+
+    // Update PDBG_DTB value
+    openpower::phal::setDevtreeEnv();
+
+    int status = 0;
+    pid_t pid = fork();
+    if (pid == 0)
+    {
+        std::string cmd("/usr/bin/attributes ");
+        cmd += "export ";
+        cmd += DEVTREE_EXPORT_FILTER_FILE;
+        cmd += " > ";
+        cmd += DEVTREE_EXP_FILE;
+        execl("/bin/sh", "sh", "-c", cmd.c_str(), 0);
+
+        auto error = errno;
+        log<level::ERR>(fmt::format("Error occurred during attributes function "
+                                    "execution, errno({})",
+                                    error)
+                            .c_str());
+        // Creating PEL at one place.
+    }
+    else if (pid > 0)
+    {
+        waitpid(pid, &status, 0);
+        if (WEXITSTATUS(status))
+        {
+            log<level::ERR>("Failed to collect attribute export data");
+            openpower::pel::createPEL(
+                "org.open_power.PHAL.Error.devtreeBackup");
+        }
+    }
+    else
+    {
+        log<level::ERR>("exportDevtree fork() failed");
+        std::runtime_error("exportDevtree fork() failed");
+    }
+}
 
 } // namespace fwupdate
 } // namespace phal
