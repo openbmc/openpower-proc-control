cb031b6076d0dfe2962245b019c6c43772103927
Jayanth Othayoth <ojayanth@in.ibm.com>
2021-11-12 02:55:49 -0600

    PHAL: Guard related service action support for SBE logs
    Added required fields in the PEL user data section to create
    guard record entries for PHAL specific error logs
    
    Tested: verified guard logs.
    
    Signed-off-by: Jayanth Othayoth <ojayanth@in.ibm.com>
    Change-Id: Ie3c190e1cdd75bb8bee113f2d34025499f24d3fa

--- extensions/phal/phal_error.cpp
+++ extensions/phal/phal_error.cpp
@@ -458,6 +458,9 @@ void processBootError(bool status)
                          }
                          jsonCalloutData["Deconfigured"] = cdg_tgt.deconfigure;
                          jsonCalloutData["Guarded"] = cdg_tgt.guard;
+                         jsonCalloutData["GuardType"] = cdg_tgt.guard_type;
+                         jsonCalloutData["EntityPath"] =
+                             cdg_tgt.target_entity_path;
 
                          jsonCalloutDataList.emplace_back(jsonCalloutData);
                      });
